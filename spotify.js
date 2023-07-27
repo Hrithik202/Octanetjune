31eb1c412d0cf9df58dcda9e4c57f0d0da8d0126
Kamlesh Joshi <hritik7374@gmail.com>
2023-07-27 19:03:26 +0530

    spotify clone

---
+++ spotify.js
@@ -0,0 +1,28 @@
+let play = document.getElementsByClassName('play')
+function playsongs() {
+    console.log("Your song is playing ")
+    if ((replace1, replace2, replace3, replace4, replace5, replace6, replace7, replace8).innerHTML == 'play') {
+        replace1.innerHTML = 'stop';
+        replace2.innerHTML = 'stop';
+        replace3.innerHTML = 'stop';
+        replace4.innerHTML = 'stop';
+        replace5.innerHTML = 'stop';
+        replace6.innerHTML = 'stop';
+        replace7.innerHTML = 'stop';
+        replace8.innerHTML = 'stop';
+        hritik.play();
+
+    }
+
+    else if ((replace1, replace2, replace3, replace4, replace5, replace6, replace7, replace8).innerHTML == 'stop') {
+        replace1.innerHTML = 'play';
+        replace2.innerHTML = 'play';
+        replace3.innerHTML = 'play';
+        replace4.innerHTML = 'play';
+        replace5.innerHTML = 'play';
+        replace6.innerHTML = 'play';
+        replace7.innerHTML = 'play';
+        replace8.innerHTML = 'play';
+        hritik.pause();
+    }
+}
